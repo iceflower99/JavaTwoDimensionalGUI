135f9be437d21083717eb3bff2b65d1217571d60
iceflower99 <mhal4767@740s-pl-1280.local>
2016-02-23 07:57:24 -0700

    aded some stuff

--- src/gui/contoller/GuiController.java
+++ src/gui/contoller/GuiController.java
@@ -1,6 +1,31 @@
 package gui.contoller;
 
+import gui.view.GUIFrame;
+
+/**
+ * GUIController for the twoDimensional GUI
+ * @author mhal4767
+ *@ version 0.x February 2016
+ */
+
 public class GuiController
 {
-
+/**
+ * reference to the GUIFrame object for the internal use 
+ */
+	private GUIFrame baseFrame;
+	/**
+	 * Creates a GUIController and initializes the GUIFrame.
+	 */
+	
+	public  GUIController()
+	{
+		baseFrame =new GUIFrame(this);
+	}
+	
+	public void start()
+	{
+		
+	}
+	
 }
---
+++ src/gui/contoller/GuiRunner.java
@@ -0,0 +1,14 @@
+package gui.contoller;
+
+
+public class GuiRunner
+{
+	public static void main (String []args)
+	{
+		guiController myguiController = new guiController();
+		 guiController.start();
+		
+		
+	}
+
+}
